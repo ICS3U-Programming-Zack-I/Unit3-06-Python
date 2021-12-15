5a18f256fb5cb855f8d7fcf5da4b3fb2ab1bb135
Ubuntu <ubuntu@ip-172-31-87-46.ec2.internal>
2021-12-15 18:05:39 +0000

    try catch.py

---
+++ num_guess_try_catch.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2019
+# This program uses a try statement
+
+import random
+
+
+def main():
+    # this function uses a try statement
+    random_num = random.randint(1, 10)
+
+    guess_string = input("Guess a number between 1 and 10: ")
+    print("")
+
+    # process & output
+    try:
+        guess_number = int(guess_string)
+        if guess_number == random_num:
+            print("Well done")
+        else:
+            print("OOh so close")
+            print("The correct number was {}".format(random_num))
+    except Exception:
+        print("This was not an integer")
+    finally:
+        print("Thanks for playing")
+
+
+if __name__ == "__main__":
+    main()
